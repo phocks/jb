f5350b576b7e9e7f959f26f4820d107fff2a3f2e
Joshua Byrd <phocks@gmail.com>
2024-12-27 14:53:53 +1000

    Add some dummy tests

---
+++ test.ts
@@ -0,0 +1,23 @@
+import { assertEquals } from "jsr:@std/assert";
+
+Deno.test("simple test", () => {
+  const x = 1 + 2;
+  assertEquals(x, 3);
+});
+
+import { delay } from "jsr:@std/async";
+
+Deno.test("async test", async () => {
+  const x = 1 + 2;
+  await delay(500);
+  assertEquals(x, 3);
+});
+
+Deno.test({
+  name: "read file test",
+  permissions: { read: true },
+  fn: () => {
+    const data = Deno.readTextFileSync("./somefile.txt");
+    assertEquals(data, "expected content");
+  },
+});
